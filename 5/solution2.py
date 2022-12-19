051922b7c2dc41f5f9458ae3447066e74aaa8ea8
Jackatear <jackatear@gmail.com>
2022-12-19 10:49:00 +0000

    accidentally misread the question so wrote day 5 part 2 (solution2.py) first and wondered why I wasnt getting the right answer. Both correct now

---
+++ 5/solution2.py
@@ -0,0 +1,72 @@
+import re
+
+def instruct(arr, n, x, y):
+    n = int(n)
+    x = int(x)
+    y = int(y)
+    
+    x = x-1 
+    y = y-1
+    
+    moving = []
+    
+    # remove n elements from x
+    for i in range(n):
+        moving.append(arr[x].pop())
+        
+    moving = moving[::-1]
+    
+    # Put back in correct order
+    for i in range(len(moving)):
+        arr[y].append(moving[i]) 
+
+
+boxes = []
+horizontal_boxes = []
+vertical_boxes = []
+
+with open("input.txt", "r") as f:
+    for i in range(0, 8):
+        boxes.append(f.readline())
+        
+    for i in range(len(boxes)):
+        boxes[i] = f"{boxes[i][0:-1]} "
+
+    index = 0
+    for box in boxes:
+        horizontal_boxes.append([])
+        for i in range(0, len(box) - 2, 4):
+            string = box[i:i+4]
+            horizontal_boxes[index].append(string.strip())
+        index += 1
+
+    vertical_boxes = [[] for x in range(0, 9)]
+    
+    for i in range(9):
+        for j in range(8):
+            vertical_boxes[i].append(horizontal_boxes[j][i])
+        vertical_boxes[i] = vertical_boxes[i][::-1]
+
+    for i in range(9):
+        while '' in vertical_boxes[i]:
+            vertical_boxes[i].remove('')
+            
+    # read instructions.
+    # all instructions start with m...
+    print('---')
+    for line in vertical_boxes:
+        print(line)
+    
+    for line in f:
+        if line[0] == 'm':
+            instructions = re.findall('[0-9]+', line)
+            n, x, y = instructions
+            instruct(vertical_boxes, n, x, y)
+            
+    print(())
+    for line in vertical_boxes:
+        print(line[-1])
+    
+    
+    
+    
